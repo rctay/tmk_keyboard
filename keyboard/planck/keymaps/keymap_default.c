60bdb8b608288dea131613189fc9827be4e1bce2
Tay Ray Chuan <rctay89@gmail.com>
2015-09-08 01:58:31 +0800

    right space is ctrl

--- keyboard/planck/keymaps/keymap_default.c
+++ keyboard/planck/keymaps/keymap_default.c
@@ -7,7 +7,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSPC},
   {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT},
-  {M(0), KC_LCTL, KC_LALT, KC_LGUI, FUNC(2),    KC_SPC,   KC_SPC,    FUNC(1),   KC_END, KC_PGDN, KC_UP,  KC_RGHT}
+  {M(0), KC_LCTL, KC_LALT, KC_LGUI, FUNC(2),    KC_SPC,   KC_RCTL,    FUNC(1),   KC_END, KC_PGDN, KC_UP,  KC_RGHT}
                                                 // Space is repeated to accommadate for both spacebar wiring positions
 },
 [1] = { /* Colemak */
