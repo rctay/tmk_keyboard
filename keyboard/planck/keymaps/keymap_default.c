872c2eacc381d757ebeb39d9d8a8b5ff89f3521a
Tay Ray Chuan <rctay89@gmail.com>
2015-09-08 02:11:49 +0800

    trigger comma/dot deadkey layers

--- keyboard/planck/keymaps/keymap_default.c
+++ keyboard/planck/keymaps/keymap_default.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 // Space is repeated to accommadate for both spacebar wiring positions
 },
 [1] = { /* Dvorak */
-  {KC_TRNS,  KC_QUOT,    KC_COMM,    KC_DOT,    KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L, KC_TRNS},
+  {KC_TRNS,  KC_QUOT,    FUNC(5),    FUNC(6),    KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L, KC_TRNS},
   {KC_TRNS,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,     KC_MINS},
   {KC_TRNS, KC_SCLN,    KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W, KC_V,  KC_Z, KC_TRNS},
   {KC_FN3, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS}
