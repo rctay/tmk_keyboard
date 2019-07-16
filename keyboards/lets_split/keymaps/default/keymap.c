7988bc078a8bae801eea09e65126d65357be39a9
Ray <rctay89@gmail.com>
2019-07-16 10:44:24 +0800

    Use X3/X4 per original dactyl keymap

--- keyboards/lets_split/keymaps/default/keymap.c
+++ keyboards/lets_split/keymaps/default/keymap.c
@@ -41,8 +41,8 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY] = LAYOUT(
         KC_1  , KC_2  , KC_3  , KC_4  , KC_5  , KC_EQL,     KC_MINS, KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,
-        KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,DF(_DVORAK) ,     KC_BSLS, KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,
-        KC_A  , KC_S  , KC_D  , KC_F  , KC_G  , ADJUST,      KC_ENT, KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,
+        KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  , KC_X3 ,     KC_BSLS, KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,
+        KC_A  , KC_S  , KC_D  , KC_F  , KC_G  , KC_X4 ,      KC_ENT, KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,
         KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,KC_LSFT,      KC_GRV, KC_N  , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,
        KC_ESCT,KC_LALT,KC_LGUI,KC_LEFT,KC_RIGHT,KC_BSPC,     KC_SPC,KC_LEFT,KC_RIGHT,KC_UP ,KC_DOWN,KC_QUOT,
                     //+--61---+-------+-------+-------+    +-------+-------+---63--+-------+
@@ -62,8 +62,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_DVORAK] = LAYOUT(
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-        KC_A  , KC_O  , KC_E  , KC_U  , KC_I  , ADJUST,     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,DF(_QWERTY),     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+        KC_A  , KC_O  , KC_E  , KC_U  , KC_I  ,KC_TRNS,     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                        KC_TRNS,KC_TRNS,                                     KC_TRNS,KC_TRNS,
                                        KC_TRNS,KC_TRNS,     KC_TRNS,KC_TRNS,
