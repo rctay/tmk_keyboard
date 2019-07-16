c2e2711f18a536a1d31a33fa418f35d6c68b5be9
Ray <rctay89@gmail.com>
2019-07-16 13:53:56 +0800

    Add dvorak keys

--- keyboards/lets_split/keymaps/default/keymap.c
+++ keyboards/lets_split/keymaps/default/keymap.c
@@ -61,9 +61,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_DVORAK] = LAYOUT(
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-        KC_A  , KC_O  , KC_E  , KC_U  , KC_I  ,KC_TRNS,     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+       KC_QUOT,KC_COMM, KC_DOT, KC_P  , KC_Y  ,KC_TRNS,     KC_TRNS, KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,
+        KC_A  , KC_O  , KC_E  , KC_U  , KC_I  ,KC_TRNS,     KC_TRNS, KC_D  , KC_H  , KC_T  , KC_N  , KC_S  ,
+       KC_SCLN, KC_Q  , KC_J  , KC_K  , KC_X  ,KC_TRNS,     KC_TRNS, KC_B  , KC_M  , KC_W  , KC_V  , KC_Z  ,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                        KC_TRNS,KC_TRNS,                                     KC_TRNS,KC_TRNS,
                                        KC_TRNS,KC_TRNS,     KC_TRNS,KC_TRNS,
