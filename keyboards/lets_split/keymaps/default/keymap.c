d565b12f798b0e291b8e5479639b35c91c424612
Ray <rctay89@gmail.com>
2019-07-16 10:38:49 +0800

    Remove colemak

--- keyboards/lets_split/keymaps/default/keymap.c
+++ keyboards/lets_split/keymaps/default/keymap.c
@@ -5,7 +5,6 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
-#define _COLEMAK 1
 #define _DVORAK 2
 #define _LOWER 3
 #define _RAISE 4
@@ -21,7 +20,6 @@ enum {
 };
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  COLEMAK,
   DVORAK,
   FN3,
 //  FN4,
@@ -119,11 +117,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case QWERTY:
       if (record->event.pressed) {
         set_single_persistent_default_layer(_QWERTY);
-      }
-      return false;
-    case COLEMAK:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_COLEMAK);
       }
       return false;
     case DVORAK:
