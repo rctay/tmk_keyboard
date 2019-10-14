cccc946ec14ab040d0efa37f022f33a8ad8a0b8b
Ray <rctay89@gmail.com>
2019-10-14 17:49:41 +0800

    Put Shift on bottom corner, grave on right edge for left hand

--- keyboards/lets_split/keymaps/default/keymap.c
+++ keyboards/lets_split/keymaps/default/keymap.c
@@ -46,10 +46,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_7  , KC_5  , KC_3  , KC_1  , KC_9  , KC_ESC,     KC_F12 , KC_0  , KC_2  , KC_4  , KC_6  , KC_8  ,
         KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,KC_MINS,     KC_EQL , KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,
         KC_A  , KC_S  , KC_D  , KC_F  , KC_G  , KC_TAB,     KC_SLSH, KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,
-        KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,KC_LSFT,     KC_RALT, KC_N  , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,
+        KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  , KC_GRV,     KC_RALT, KC_N  , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,
                     //+--61---+-------+-------+-------+    +-------+-------+---63--+-------+
                     //+--6.3--+-------+-------+-------+    +-------+-------+--6.9--+-------+
-                        KC_GRV,KC_LALT,                                     KC_RCTL,KC_ENT ,
+                       KC_LSFT,KC_LALT,                                     KC_RCTL,KC_ENT ,
                                     //+--65---+-------+    +--66---+-------+
                                     //+--6.5--+-------+    +--6.7--+-------+
                                         KC_X3 ,KC_LGUI,     KC_RSFT, KC_X4 ,
